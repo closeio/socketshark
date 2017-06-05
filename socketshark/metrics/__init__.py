8e04f66c53a1cb640582852edad8b56507763f25
Joe Kemp <joe@close.io>
2017-06-05 08:55:00 -0400

    Add connection count

--- socketshark/metrics/__init__.py
+++ socketshark/metrics/__init__.py
@@ -25,6 +25,10 @@ class Metrics:
             self.shark.log.info('initializing metrics', provider=name)
             provider.initialize()
 
+    def increase_connection_count(self):
+        for provider in self.providers.values():
+            provider.increase_connection_count()
+
     def set_ready(self, ready):
         for provider in self.providers.values():
             provider.set_ready(ready)
--- socketshark/metrics/log.py
+++ socketshark/metrics/log.py
@@ -4,12 +4,17 @@ class LogMetrics:
     """
     def __init__(self, shark, config):
         self.shark = shark
+        self.connection_count = 0
 
     def initialize(self):
         pass
 
     def set_ready(self, ready):
         self.shark.log.debug('metrics', ready=ready)
+
+    def increase_connection_count(self):
+        self.connection_count += 1
+        self.shark.log.debug('metrics', count=self.connection_count)
 
     def set_connection_count(self, count):
         self.shark.log.debug('metrics', connection_count=count)
