879cc5dbbd70ecb1424e96d6651bcb0cf554e86a
Thomas Steinacher <tom@eggdrop.ch>
2017-06-07 12:55:35 -0700

    Lint

--- socketshark/metrics/log.py
+++ socketshark/metrics/log.py
@@ -15,14 +15,16 @@ class LogMetrics:
 
     def decrease_connection_count(self):
         self.active_connections -= 1
-        self.shark.log.debug('metrics', active_connections=self.active_connections,
-                                        total_connections=self.connection_count)
+        self.shark.log.debug('metrics',
+                             active_connections=self.active_connections,
+                             total_connections=self.connection_count)
 
     def increase_connection_count(self):
         self.connection_count += 1
         self.active_connections += 1
-        self.shark.log.debug('metrics', active_connections=self.active_connections,
-                                        total_connections=self.connection_count)
+        self.shark.log.debug('metrics',
+                             active_connections=self.active_connections,
+                             total_connections=self.connection_count)
 
     def log_event(self, event, success):
         self.shark.log.debug('metrics', evt=event, success=success)
