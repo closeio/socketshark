ab2bbf696253d9702b94f6f694eec89d77c6bd23
Thomas Steinacher <tom@eggdrop.ch>
2017-06-07 12:50:05 -0700

    Fix logging metrics backend

--- socketshark/metrics/log.py
+++ socketshark/metrics/log.py
@@ -14,17 +14,15 @@ class LogMetrics:
         self.shark.log.debug('metrics', ready=ready)
 
     def decrease_connection_count(self):
-        self.connection_count += 1
         self.active_connections -= 1
-        self.shark.log.debug('metrics', total_connections=self.connection_count)
+        self.shark.log.debug('metrics', active_connections=self.active_connections,
+                                        total_connections=self.connection_count)
 
     def increase_connection_count(self):
         self.connection_count += 1
         self.active_connections += 1
-        self.shark.log.debug('metrics', total_connections=self.connection_count)
-
-    def set_active_connection_count(self, count):
-        self.shark.log.debug('metrics', active_connections=count)
+        self.shark.log.debug('metrics', active_connections=self.active_connections,
+                                        total_connections=self.connection_count)
 
     def log_event(self, event, success):
         self.shark.log.debug('metrics', evt=event, success=success)
