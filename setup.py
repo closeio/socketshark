a6c132af6a9a7f7c9a4b31ad62b597cd24fef00e
Stefan Wojcik <wojcikstefan@gmail.com>
2021-08-25 11:35:32 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = install_requires + [
 
 setup(
     name='socketshark',
-    version='0.2.1',
+    version='0.2.2',
     url='http://github.com/closeio/socketshark',
     license='MIT',
     description='WebSocket message router',
