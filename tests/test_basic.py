0e719e7abd348a46da169ac9dca14f138195f335
Danilo Šegan <danilo.segan@close.io>
2021-12-03 16:32:17 +0100

    Introduce isort configuration.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -10,9 +10,12 @@ import pytest
 from aioresponses import aioresponses
 from yarl import URL
 
-from socketshark import SocketShark, config_defaults
-from socketshark import constants as c
-from socketshark import setup_logging
+from socketshark import (
+    SocketShark,
+    config_defaults,
+    constants as c,
+    setup_logging,
+)
 from socketshark.session import Session
 
 LOCAL_REDIS_HOST = os.environ.get('LOCAL_REDIS_HOST')
