963501c6338f9a248c6e08c39518380a7e3d06bd
Stefan Wojcik <wojcikstefan@gmail.com>
2026-04-27 15:04:59 +0200

    Type-annotate the re-added `LogMetrics`, too.
    I briefly removed it from master before realizing it's still used (in a very
    counter-intuitive fashion).

--- socketshark/metrics/log.py
+++ socketshark/metrics/log.py
@@ -1,20 +1,30 @@
+from typing import TYPE_CHECKING
+
+from ..types import MetricsProviderConfig
+
+if TYPE_CHECKING:
+    from .. import SocketShark
+
+
 class LogMetrics:
     """
     Log metrics provider that prints each metric to the log.
     """
 
-    def __init__(self, shark, config):
+    def __init__(
+        self, shark: 'SocketShark', config: MetricsProviderConfig
+    ) -> None:
         self.shark = shark
         self.connection_count = 0
         self.active_connections = 0
 
-    def initialize(self):
+    def initialize(self) -> None:
         pass
 
-    def set_ready(self, ready):
+    def set_ready(self, ready: bool) -> None:
         self.shark.log.debug('metrics', ready=ready)
 
-    def decrease_connection_count(self):
+    def decrease_connection_count(self) -> None:
         self.active_connections -= 1
         self.shark.log.debug(
             'metrics',
@@ -22,7 +32,7 @@ class LogMetrics:
             total_connections=self.connection_count,
         )
 
-    def increase_connection_count(self):
+    def increase_connection_count(self) -> None:
         self.connection_count += 1
         self.active_connections += 1
         self.shark.log.debug(
@@ -31,5 +41,5 @@ class LogMetrics:
             total_connections=self.connection_count,
         )
 
-    def log_event(self, event, success):
+    def log_event(self, event: str, success: bool | None) -> None:
         self.shark.log.debug('metrics', evt=event, success=success)
